92012443fa397b6c4b99b3b8f9e8158fe63fbebc
MacWilliam <874681044@qq.com>
2020-04-25 20:07:55 +0800

    4

--- egs/timit/v1/run.sh
+++ egs/timit/v1/run.sh
@@ -32,14 +32,22 @@ timit_root=/data/timit
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_fb24
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_fb24
 #datafrom=py24
-
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_fix
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_fix
 #datafrom=dpy24_fix
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_var
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_var
+#datafrom=dpy24_var
 
-train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_var
-test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_var
-datafrom=dpy24_var
+train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_fb30
+test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_fb30
+datafrom=py30
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_var
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_var
+#datafrom=dpy24_var
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_fix
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_fix
+#datafrom=dpy24_fix
 
 # train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_fb40_dnn_20
 # test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_fb40_dnn_20
@@ -260,3 +268,9 @@ fi
 #EER: 3.73%
 #minDCF(p-target=0.01): 0.5288
 #minDCF(p-target=0.001): 0.7901
+
+#20200425 19:42
+#py24 with mean weight from var 512GMMs 128
+#EER: 3.638%
+#minDCF(p-target=0.01): 0.5046
+#minDCF(p-target=0.001): 0.8105
