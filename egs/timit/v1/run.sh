752d20bf1c8a6254e60b6c85098ce8b612e20649
MacWilliam <874681044@qq.com>
2020-04-25 20:31:50 +0800

    4

--- egs/timit/v1/run.sh
+++ egs/timit/v1/run.sh
@@ -32,9 +32,9 @@ timit_root=/data/timit
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_fb24
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_fb24
 #datafrom=py24
-#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_lfb24
-#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_lfb24
-#datafrom=lpy24
+train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_lfb24
+test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_lfb24
+datafrom=lpy24
 
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb24_fix
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb24_fix
@@ -49,9 +49,9 @@ timit_root=/data/timit
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb30_var
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb30_var
 #datafrom=dpy30_var
-train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb30_fix
-test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb30_fix
-datafrom=dpy30_fix
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_dfb30_fix
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_dfb30_fix
+#datafrom=dpy30_fix
 
 # train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/train_fb40_dnn_20
 # test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/test_fb40_dnn_20
