67efa80343730d8d2a176a22ca12eda10ecc8bfb
MacWilliam <874681044@qq.com>
2020-04-27 14:00:39 +0800

    4

--- egs/librispeech/v1/run.sh
+++ egs/librispeech/v1/run.sh
@@ -26,13 +26,13 @@ libri_root=/data/libri
 
 #/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_fb24_dnn_new
 
-train=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/dev_fb24
-test=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/test_fb24
-datafrom=py24
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/dev_fb24
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/test_fb24
+#datafrom=py24
 #
-# train=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/dev_dfb24_fix
-# test=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/test_dfb24_fix
-# datafrom=dpy24_fix
+ train=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/dev_dfb24_fix
+ test=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/pyfb/test_dfb24_fix
+ datafrom=dpy24_fix
 
 # train=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/train_mfcc_20
 # test=/home/yangwenhao/local/project/lstm_speaker_verification/data/libri/test_mfcc_20
@@ -164,6 +164,12 @@ if [ $stage -le 7 ]; then
   echo "EER: $eer%"
   echo "minDCF(p-target=0.01): $mindcf1"
   echo "minDCF(p-target=0.001): $mindcf2"
+
+# VAD 5.5 0.5
+# fb24 640 UBM 128
+#EER: 2.89%
+#minDCF(p-target=0.01): 0.7239
+#minDCF(p-target=0.001): 0.9970
 
 # VAD 5.5 0.5
 # fb24 512 UBM 128
