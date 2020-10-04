fd724eabdfcaeaf5978bc17c821b4d0210077243
MacWilliam <874681044@qq.com>
2020-10-04 21:07:07 +0800

    timit power

--- egs/timit/v1/run.sh
+++ egs/timit/v1/run.sh
@@ -32,9 +32,14 @@ timit_root=/data/timit
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_fb24
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_fb24
 #datafrom=py24
-train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_lfb24
-test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_lfb24
-datafrom=lpy24
+#train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_lfb24
+#test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_lfb24
+#datafrom=lpy24
+
+train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_fb24_power
+test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_fb24_power
+datafrom=fb24_power
+
 #train=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/train_afb24
 #test=/home/yangwenhao/local/project/lstm_speaker_verification/data/timit/pyfb/test_afb24
 #datafrom=apy24
